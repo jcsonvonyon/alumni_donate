28b044071266f80ed9ec1dd249d087e42ea9dfef
dleahcodes <leahfemi@gmail.com>
2022-12-14 19:15:14 +0100

    account setup files

---
+++ js/script.js
@@ -0,0 +1,32 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    gragCursor: 'true',
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+ 
+
+  var swiper = new Swiper(".slide-content", {
+    slidesPerView: 1,
+    spaceBetween: 25,
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
